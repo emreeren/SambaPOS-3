6dcc8fbbfc1aaefc3171bc59cc21974dadd50bf6
Emre Eren <emreeren@gmail.com>
2012-03-17 09:26:33 +0200

    Implementing Account State

---
+++ Samba.Domain/Models/Accounts/AccountState.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Samba.Infrastructure.Data;
+
+namespace Samba.Domain.Models.Accounts
+{
+    public class AccountState : Entity
+    {
+        public int AccountTemplateId { get; set; }
+        public string Color { get; set; }
+    }
+}
