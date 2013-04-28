234a6b374b417fdfdba55afab511257a9b709cf8
Emre Eren <emreeren@gmail.com>
2013-04-28 16:23:58 +0300

    New flexible screen keyboard implemented

---
+++ Samba.Presentation.Controls/VirtualKeyboard/KeyboardView3.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Windows.Forms;
+using UserControl = System.Windows.Controls.UserControl;
+
+namespace Samba.Presentation.Controls.VirtualKeyboard
+{
+    /// <summary>
+    /// Interaction logic for KeyboardView.xaml
+    /// </summary>
+    public partial class KeyboardView3 : UserControl
+    {
+        public KeyboardView3()
+        {
+            InitializeComponent();
+            DataContext = new KeyboardViewModel();
+        }
+
+        private void UserControl_IsVisibleChanged(object sender, System.Windows.DependencyPropertyChangedEventArgs e)
+        {
+            if (DataContext == null || ((KeyboardViewModel)DataContext).Model == null) return;
+
+            ((KeyboardViewModel)DataContext).Model.ReleaseKey(Keys.ShiftKey);
+            ((KeyboardViewModel)DataContext).Model.ReleaseKey(Keys.LShiftKey);
+            ((KeyboardViewModel)DataContext).Model.ReleaseKey(Keys.RShiftKey);
+        }
+
+        public void SendKey(Keys key)
+        {
+            ((KeyboardViewModel)DataContext).Model.SendKey(key);
+        }
+    }
+}
